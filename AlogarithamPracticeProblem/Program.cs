965ff98ca0f980dd636e2fd2e7ae61ba1febda76
MounikaAddagatla <89588852+MounikaAddagatla@users.noreply.github.com>
2021-10-08 18:24:33 +0530

    Add: permutation of the string

---
+++ AlogarithamPracticeProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AlogarithamPracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome To Algorithem Practice problems");
+            int options;
+            bool isExit = false;
+            while (!isExit)
+            {
+                Console.WriteLine("choose 1.PermutationOfString ");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        Console.WriteLine("Enter a string");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutations.Permute(str, 0, n - 1);
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
