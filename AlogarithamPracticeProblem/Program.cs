0763bf8c34de81015219f7304771bfcccac5c6fc
MounikaAddagatla <89588852+MounikaAddagatla@users.noreply.github.com>
2021-10-08 18:42:57 +0530

    Add: Binary search with Text file

--- AlogarithamPracticeProblem/Program.cs
+++ AlogarithamPracticeProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AlogarithamPracticeProblem
 {
@@ -6,12 +7,12 @@ namespace AlogarithamPracticeProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome To Algorithem Practice problems");
+            Console.WriteLine(" Welcome To Algorithem Practice problems ");
             int options;
             bool isExit = false;
             while (!isExit)
             {
-                Console.WriteLine("choose 1.PermutationOfString ");
+                Console.WriteLine("choose \n1.PermutationOfString \n2. BubbleSort ");
                 options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
@@ -21,6 +22,27 @@ namespace AlogarithamPracticeProblem
                         int n = str.Length;
                         Permutations.Permute(str, 0, n - 1);
                         Console.ReadLine();
+                        break;
+                    case 2:
+                        string filepath = @"E:\AlgorithmsPracticeProblem\AlogarithamPracticeProblem\NewFolder\Word.txt";
+                        string text = File.ReadAllText(filepath);
+                        string[] textArray = text.Split(" ");
+                        BinarySearch binarySearch = new BinarySearch(textArray);
+                        Console.WriteLine("Enter a word to search:");
+                        string word = Console.ReadLine();
+                        int found = binarySearch.FindWord(word, 0, textArray.Length - 1);
+                        if (found == 1)
+                        {
+                            Console.WriteLine("The word {0} is found", word);
+                        }
+                        else
+                        {
+                            Console.WriteLine("the word {0} is not found", word);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Exit");
                         break;
                 }
             }
